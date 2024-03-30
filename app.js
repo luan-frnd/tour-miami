a570de2fc1868c9223ba7124a4fa678f2fc7b544
luangabriel <luan34460@gmail.com>
2024-03-30 17:46:00 -0300

    responsividadde adicioada

--- app.js
+++
@@ -1,7 +0,0 @@
-const hamburger = document.querySelector('.hamburger');
-const navbar = document.querySelector('.navbar');
-
-    hamburger.addEventListener('click', function( ) {
-        navbar.classList.toggle('active');
-    });
-
